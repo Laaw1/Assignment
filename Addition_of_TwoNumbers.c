c79233d1cc9d46ea699d83f1b8e42f834a9666e3
Laaw1 <158718607+Laaw1@users.noreply.github.com>
2024-03-28 13:24:15 +0300

    Add files via upload

---
+++ Addition_of_TwoNumbers.c
@@ -0,0 +1,17 @@
+/*
+Introduction to programing
+Addition of two numbers
+By EJO
+@Laaw1
+*/
+#include<stdio.h>
+int main()
+{
+    int x, y;
+
+    printf("Enter value of x and y\n");
+    scanf("%d %d", &x, &y);
+
+    printf("Addition of %d and %d = %d\n", x, y, (x+y));
+return 0;
+}
